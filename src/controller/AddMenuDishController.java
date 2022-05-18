1a75e6c0b87262b6cd823262dd5f65d14dbe1719
ilariacarboni <m.ilariacarboni@libero.it>
2022-05-18 17:37:14 +0200

    commit prova su newrepository

--- src/controller/AddMenuDishController.java
+++ src/controller/AddMenuDishController.java
@@ -4,18 +4,24 @@
  */
 package controller;
 
+import entity.Menu;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+
 import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
  *
- * @author milar
+ *
  */
 
     
@@ -24,22 +30,72 @@ public class AddMenuDishController implements Initializable {
     /**
      * Initializes the controller class.
      */
-   
+    
+    
+
     @FXML
-    private TextField nomeTxt;
+    private Label categoriaLbl;
+
     @FXML
-    private TextField prezzoTxt;
+    private ChoiceBox<?> categorieChoicebox;
+
+    
+
+    @FXML
+    private Button insertdishBtn;
+
     @FXML
     private Label nomeLbl;
+
+    @FXML
+    private TextField nomeTxt;
+
     @FXML
     private Label prezzoLbl;
+
     @FXML
-    private Button insertDishBtn;
+    private TextField prezzoTxt;
+
     
-    private Menu m;
+   
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        nomeTxt.textProperty().addListener((ObservableValue<? extends String> observable, String oldValue, String newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                nomeTxt.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        });
+        
+        prezzoTxt.textProperty().addListener((ObservableValue<? extends String> observable, String oldValue, String newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                prezzoTxt.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        });
+        
+        
     }    
+
+    @FXML
+    private void addMenuDishBtnClicked(ActionEvent event) {
+        
+        //controllo che siano stati inseriti tutti i campi
+        if(nomeTxt.getText().isEmpty() || prezzoTxt.getText().isEmpty() ||  ){
+            
+            System.out.println("campo vuoto");
+            
+        }else{
+		String nameDish = nomeTxt.getText();
+            	int price = Integer.parseInt(prezzoTxt.getText());
+                String category = categoriaTxt.getText(); //param fisso per categoria selezionata
+                
+
+            Menu m = new Menu (nameDish, price, category); //collego con entity del db
+            ControllerForView.getInstance().save(m);
+            System.out.println("aggiunto");
+        }
+        
+    }
+        
     
 }
